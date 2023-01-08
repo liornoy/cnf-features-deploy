dd9a54a76655ea0e0e8ddfff6e473aa550999df8
liornoy <lnoy@redhat.com>
2023-01-08 17:27:34 +0200

    Update Dockerfiles to go 1.18
    As ginkgo/v2 requires go version 1.18 this commit
    updates the Dockerfiles.

--- cnf-tests/hack/build-test-bin.sh
+++ cnf-tests/hack/build-test-bin.sh
@@ -18,7 +18,7 @@ DONT_REBUILD_TEST_BINS="${DONT_REBUILD_TEST_BINS:-false}"
 
 if ! which ginkgo; then
 	echo "Downloading ginkgo tool"
-	go install github.com/onsi/ginkgo/ginkgo
+	go install github.com/onsi/ginkgo/v2/ginkgo@v2.7.0
 fi
 
 mkdir -p bin
